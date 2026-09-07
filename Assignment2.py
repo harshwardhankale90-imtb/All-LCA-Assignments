6b013d0e05da073ce89dcdaf6405eaabcea0150b
harshwardhankale90-imtb <harshawardhankale90@gmail.com>
2026-09-07 12:44:11 +0530

    Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,1 +1,12 @@
+def check_right_angle_triangle(a,b,c):
+  if c>a and c>b and a+b>c and a*2+b*2==c*2
+  print("The triangle is right angled triangle")
+    else:
+      print("The triangle is not right angled")
 
+a=float(input("Enter first side"))
+b=float(input("Enter your second side"))
+c=float(input("Enter hypotenuse side"))
+
+
+check_right_angle_triangle(a,b,c)
